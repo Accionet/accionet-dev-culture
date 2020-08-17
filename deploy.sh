351c6e7cb40e25531054e2b3ed5f8063ecaf65ba
antonio <afontaine@uc.cl>
2020-08-17 09:47:57 -0400

    chore(deploy): scropt to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,30 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
 git checkout master
 git pull origin master
 npm run docs:build
+
+
+# navigate into the build output directory
+cd docs/.vuepress/dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://afontainec.github.io/accionet-dev-culture
+git push -f https://github.com/afontainec/accionet-dev-culture.git master:gh-pages
+
+cd ../../../
 git add .
-git commit -am "build docs"
+git commit -am "chore: build docs"
 npm version patch -m "chore: release version %s [skip ci]"
-git push heroku master
 git push --tags
 git push origin master
 git checkout dev
